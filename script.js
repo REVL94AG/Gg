67f987022b342bf2835fcc37b42b4843d147fba8
REVL94AG <abdullahalzwy94@gmail.com>
2025-08-07 00:14:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    let button1 = document.querySelector('#btn1');
+    button1.addEventListener('click', function() {
+        alert("Rating for Clair Obscur is: [9.4]");
+    });
+
+    let button2 = document.querySelector('#btn2');
+    button2.addEventListener('click', function() {
+        alert("Rating for Wuchang is: [9.1]");
+    });
+
+
+    let button3 = document.querySelector('#btn3');
+    button3.addEventListener('click', function() {
+        alert("Rating for Elden Ring:Nightreign is: [9.5]");
+    });
+
+
+    let button4 = document.querySelector('#btn4');
+    button4.addEventListener('click', function() {
+        alert("Rating for Split Fiction is: [9.4]");
+    });
+
+});
